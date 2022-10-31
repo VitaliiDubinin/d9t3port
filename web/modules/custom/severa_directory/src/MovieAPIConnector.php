a9bd530dfc835b8d5279d1dc81be69a073535aa0
Vitalii Dubinin <98089841+VitaliiDubinin@users.noreply.github.com>
2022-10-31 23:07:59 +0200

    key: deleted from MovieApiConnector

--- web/modules/custom/severa_directory/src/MovieAPIConnector.php
+++ web/modules/custom/severa_directory/src/MovieAPIConnector.php
@@ -6,7 +6,7 @@ private $client;
 private $query;
 
 public function __construct(\Drupal\Core\Http\ClientFactory $client){
-    $movie_api_config = \Drupal::state()->get(key:\Drupal\severa_directory\Form\MovieAPI::MOVIE_API_CONFIG_PAGE);
+    $movie_api_config = \Drupal::state()->get(\Drupal\severa_directory\Form\MovieAPI::MOVIE_API_CONFIG_PAGE);
     $api_url=($movie_api_config['api_base_url'])?: 'https:://api.themoviedb.org';
     $api_key=($movie_api_config['api_key'])?:'8804da8efecd504320eb35f0438ea339';
 
