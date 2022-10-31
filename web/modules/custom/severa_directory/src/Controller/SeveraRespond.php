76f7d60b827451539f0f7ee5735b82c1ac5f7995
Vitalii Dubinin <98089841+VitaliiDubinin@users.noreply.github.com>
2022-10-31 17:19:26 +0200

    movie created

--- web/modules/custom/severa_directory/src/Controller/SeveraRespond.php
+++ web/modules/custom/severa_directory/src/Controller/SeveraRespond.php
@@ -6,14 +6,57 @@ use Drupal\Core\Controller\ControllerBase;
 
 class SeveraRespond extends ControllerBase {
     public function view() {
+      $this->listMovies();
         $content = [];
     
         $content['name'] = 'My name is Severa';
+        $content['movies']=$this->createMovieCard();
     
         return [
           '#theme' => 'severa-respond',
           '#content' => $content,
         ];
+      }
+
+      public function listMovies(){
+        /** @var \MovieAPIConnector $severa_api_connector_service */
+        $movie_api_connector_service =\Drupal::service(id:'severa_directory.api_connector');
+        $movie_list = $severa_api_connector_service->discoverMovies();
+        if(!empty($movie_list -> results)){
+          return $movie_list->results;
+        }
+        return[];
+      }
+
+      public function createMovieCard(){
+        $movie_api_connector_service=\Drupal::service(id:'severa_directory.api_connector');
+
+        $movieCards=[];
+        $movies = $this->listMovies();
+        if(!empty($movies)){
+          foreach ($movies as $movie){
+
+            $content =[
+              'title'=>$movie->title,
+              'description'=>$movie->overview,
+              'movie_id'=>$movie->id,
+
+            ];
+
+            $movieCards[]=[
+              '#theme'=>'movie-card',
+              '#content'=>$content,
+
+
+            ];
+
+          
+          }
+        }
+
+        return $movieCards;
+
+
       }
 
 }
